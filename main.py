4e422d4d8c5234ceb859c7df8fec214abc84e754
Indranil Saha <sahaindranil2004@gmail.com>
2022-10-31 03:31:58 +0530

    updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import cv2
 import numpy as np
 import face_recognition
@@ -25,6 +26,20 @@ def findEncodings(images):
     return encodeList
 
 
+def markAttandance(name):
+    with open('Attandance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        print(myDataList)
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
 encodeListKnown = findEncodings(images)
 print("Encoding Complete")
 
@@ -33,11 +48,11 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     # imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    img = imgSmall
-    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+    imgCopy = img
+    imgCopy = cv2.cvtColor(imgCopy, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgSmall)
-    encodesCurFrame = face_recognition.face_encodings(imgSmall, facesCurFrame)
+    facesCurFrame = face_recognition.face_locations(imgCopy)
+    encodesCurFrame = face_recognition.face_encodings(imgCopy, facesCurFrame)
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
@@ -54,6 +69,7 @@ while True:
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            markAttandance('Elon')
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
