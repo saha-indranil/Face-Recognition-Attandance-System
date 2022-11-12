b16d2682e4169c1f075ac20e6a884b60d1206032
Indranil Saha <sahaindranil2004@gmail.com>
2022-11-12 17:37:41 +0530

    Updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import face_recognition
 import os
 
-path = 'project_images'
+path = 'Attandance Images'
 images = []
 classNames = []
 myList = os.listdir(path)
@@ -27,7 +27,7 @@ def findEncodings(images):
 
 
 def markAttandance(name):
-    with open('Attandance.csv', 'r+') as f:
+    with open('attandance.csv', 'r+') as f:
         myDataList = f.readlines()
         print(myDataList)
         nameList = []
@@ -47,7 +47,6 @@ print("Encoding Complete")
 cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
-    # imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgCopy = img
     imgCopy = cv2.cvtColor(imgCopy, cv2.COLOR_BGR2RGB)
 
@@ -57,19 +56,18 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
+        # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name)
             y1, x2, y2, x1 = faceLoc
-            # y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            markAttandance('Elon')
+            markAttandance(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
